fda8c79ce03cf2ff751180d9694061581f5a6ac8
itslkmgcp <lochan.marwah@springernature.com>
2021-04-10 17:49:19 +0530

    Adding flag for install

--- install_docker_postgresql.sh
+++ install_docker_postgresql.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 sudo apt update
 
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 sudo systemctl status docker
 
